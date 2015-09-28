97663b2bbd960bdebc8157d105908638dcb68706
Seiei Miyagi <hanachin@gmail.com>
2015-09-28 22:58:38 +0900

    initial commit

---
+++ abort.rb
@@ -0,0 +1,4 @@
+require 'jaro_winkler'
+
+JaroWinkler.c_distance("In that case, you can set the `config.force_ssl` in your environment file.", "In this guide you will learn how controllers work and how they fit into the request cycle in your application.")
+
